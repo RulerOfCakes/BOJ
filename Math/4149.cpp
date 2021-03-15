350d67b26535ff3591152cf39cc7f81c89822205
RulerOfCakes <fragmentbombs@gmail.com>
2021-03-16 00:53:32 +0900

    boj 4149 - pollard-rho. I want to die.

---
+++ Math/4149.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+//pollard's rho algorithm
+using namespace std;
+using ll = long long;
+ll N;
+vector<ll> alist = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41};
+//x*y % m, overflow safe calculation
+ll mulmod(ll p, ll q, ll m){
+    p %= m;
+    q %= m;
+    ll r = 0;
+    ll w = p;
+    while(q){
+        if(q%2){
+            r = (r+w)%m;
+        }
+        w = (w+w)%m;
+        q >>= 1;
+    }
+    return r;
+}
+
+//x^y % m, overflow safe calculation
+ll powmod(ll x, ll y, ll m){
+    x %= m;
+    ll r = 1ll;
+    while(y>0){
+        if(y&1)r = mulmod(r, x, m);
+        x = mulmod(x, x, m);
+        y /= 2;
+    }
+    return r;
+}
+
+bool miller_rabin(ll n, ll a) {
+    if(!n%a)return false;
+    ll d = n-1;
+    while(true){
+        ll tmp = powmod(a, d, n);
+        if(d&1)return (tmp != 1 && tmp != n-1);
+        else if (tmp == n-1)return false;
+        d >>= 1;
+    }
+}
+
+bool prime_test(ll n){
+    if(n <= 1)return false;
+    for(ll a : alist){
+        if(n == a)return true;
+        if(n > 40 && miller_rabin(n, a))return false;
+    }
+    if(n <= 40)return false;
+    return true;
+}
+
+ll gcd(ll a, ll b){
+    if(b==0)return a;
+    else return gcd(b, a%b);
+}
+
+vector<ll> factors;
+void factorize(ll n){
+    if(n==1)return;
+    //VERY IMPORTANT IF STATEMENT, THIS ONE COST ME 2 HOURS WORTH OF SANITY
+    if(n%2==0){
+        factors.push_back(2);
+        factorize(n/2);
+        return;
+    }
+    if(prime_test(n)){
+        factors.push_back(n);
+        return;
+    }
+    ll x, y, c, g = n;
+    //f(x) = (x^2 + c) mod n
+    auto f = [&](ll x){
+        return (mulmod(x, x, n)+c)%n;
+    };
+    do{
+        if(g==n){
+            x = y = rand() % (n-2) + 2;
+            c = rand() % 20 + 1;
+            g = 1;
+        }
+        x = f(x);
+        y = f(f(y));
+        g = gcd(abs(x-y), n);
+    } while (g == 1);
+    factorize(g);
+    factorize(n/g);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> N;
+    
+    factorize(N);
+    sort(factors.begin(), factors.end());
+    for(auto a : factors){
+        cout << a << '\n';
+    }
+}
