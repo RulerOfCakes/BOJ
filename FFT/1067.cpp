ddd663f0716b8959ac2acea211c9d077d60a95b2
RulerOfCakes <fragmentbombs@gmail.com>
2021-04-01 18:37:00 +0900

    boj 1067 - FFT

--- FFT/1067.cpp
+++ FFT/1067.cpp
@@ -2,15 +2,52 @@
 
 using namespace std;
 using base = complex<double>;
+using ll = long long;
 int N;
-vector<int> v;
-void fft(){
-    if(v.size()==1)return;
 
-    vector<base> w;
-    for(int i = 0; i <= N/2; i++){
-        w.push_back(exp(imag(2)*M_PI*i/N));
+void fft(vector<base> &a, bool inv) {
+    int n = (int) a.size();
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        while (!((j ^= bit) & bit)) bit >>= 1;
+        if (i < j) swap(a[i], a[j]);
     }
+    for (int i = 1; i < n; i <<= 1) {
+        double x = inv ? M_PI / i : -M_PI / i;
+        base w = {cos(x), sin(x)};
+        for (int j = 0; j < n; j += i << 1) {
+            base th = {1, 0};
+            for (int k = 0; k < i; k++) {
+                base tmp = a[i + j + k] * th;
+                a[i + j + k] = a[j + k] - tmp;
+                a[j + k] += tmp;
+                th *= w;
+            }
+        }
+    }
+    if (inv) {
+        for (int i = 0; i < n; i++) {
+            a[i] /= n;
+        }
+    }
+}
+
+vector<ll> multiply(vector<ll> &y, vector<ll> &z) {
+    vector<base> a(y.begin(), y.end()), b(z.begin(), z.end());
+    int n = 2;
+    while(n<y.size()+z.size())n<<=1;
+    a.resize(n); b.resize(n);
+    fft(a, false);
+    fft(b, false);
+    for(int i = 0; i < n; i++){
+        a[i] *= b[i];
+    }
+    fft(a, true);
+    vector<ll> ret(n);
+    for (int i = 0; i < n; i++) {
+        ret[i] = (ll)round(a[i].real());
+    }
+    return ret;
 }
 
 int main()
@@ -19,5 +56,18 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> N;
-    
+    vector<ll> a(N*2), b(N);
+    for(int i = 0; i < N; i++){
+        cin >> a[i];
+        a[i+N]=a[i];
+    }
+    for(int i = 0; i < N; i++){
+        cin>>b[N-i-1];
+    }
+    vector<ll> res = multiply(a,b);
+    ll ans = 0;
+    for(auto t : res){
+        ans = max(t, ans);
+    }
+    cout << ans << '\n';
 }
