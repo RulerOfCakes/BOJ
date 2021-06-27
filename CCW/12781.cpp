7f02af83db22264e84e478e14d91b9ded43240bd
RulerOfCakes <fragmentbombs@gmail.com>
2021-06-27 21:18:01 +0900

    boj 12781 - line intersection in convex context

---
+++ CCW/12781.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using pi = pair<int, int>;
+int ccw(pi p1, pi p2, pi p3){
+    int sol = (p1.first*p2.second + p2.first*p3.second + p3.first*p1.second) - (p1.second*p2.first + p2.second*p3.first + p3.second*p1.first);
+    if(sol<0)return -1;
+    else if(sol == 0)return 0;
+    else return 1;
+}
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    pi p1, p2, p3, p4;
+    cin >> p1.first >> p1.second >> p2.first >> p2.second >> p3.first >> p3.second >> p4.first >> p4.second;
+    if(ccw(p1, p2, p3)*ccw(p1,p2,p4)<0){
+        cout << 1;
+    }
+    else cout << 0;
+}
