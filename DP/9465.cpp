ebc0b0eb0b007501077bc038953715aa908c1996
RulerOfCakes <fragmentbombs@gmail.com>
2021-07-25 12:17:32 +0900

    FIXED: exception on N==1||N==2

--- DP/9465.cpp
+++ DP/9465.cpp
@@ -23,7 +23,11 @@ int main(){
             cin >> arr[i][1];
         }
         dp[0][0] = arr[0][0];dp[0][1] = arr[0][1];
+        ans=max(dp[0][0],dp[0][1]);
+        
         dp[1][0] = arr[1][0] + arr[0][1];dp[1][1] = arr[1][1] + arr[0][0];
+        ans=max(ans, max(dp[1][1],dp[1][0]));
+        
         for(int i = 2; i < N; i++){
             dp[i][0] = max(dp[i-1][1], dp[i-2][1]) + arr[i][0];
             dp[i][1] = max(dp[i-1][0], dp[i-2][0]) + arr[i][1];
