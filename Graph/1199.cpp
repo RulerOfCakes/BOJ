e372afbfb5e0fdb12300279edd67deb4c5161344
RulerOfCakes <fragmentbombs@gmail.com>
2021-07-26 20:13:21 +0900

    FIX: bad implementation of Hierholzer's algorithm

--- Graph/1199.cpp
+++ Graph/1199.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int N;
 int arr[1002][1002];
-int start;
+int pos[1002];//current check position in dfs
 
 bool check(){
     for(int i = 1; i <= N; i++){
@@ -12,14 +12,14 @@ bool check(){
             cnt += arr[i][j];
         }
         if(cnt&1)return 0;
-        if(!start&&cnt)start=i;
+        
     }
     return 1;
 }
 
 void dfs(int now){
-    for(int i = 1; i <= N; i++){
-        if(arr[now][i]){
+    for(int &i = pos[now]; i <= N; i++){
+        while(arr[now][i]){
             arr[now][i]--;
             arr[i][now]--;
             dfs(i);
@@ -34,16 +34,17 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> N;
-    for (int i = 0; i < N; i++)
+    for (int i = 1; i <= N; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = 1; j <= N; j++)
         {
-            cin >> arr[i+1][j+1];
+            cin >> arr[i][j];
         }
     }
     if(!check()){
         cout << -1;
         return 0;
     }
-    dfs(start);
+    for(int i = 1; i <= N; i++)pos[i]=1;
+    dfs(1);
 }
