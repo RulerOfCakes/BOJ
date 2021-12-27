9cc8b781e2f9d194413e9a548667e863763784e1
RulerOfCakes <fragmentbombs@gmail.com>
2021-12-27 16:23:33 +0900

    boj 10719 - dp

---
+++ DP/10719.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T,N,M,C;
+int dp[101010][31];
+int mins[101010];
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    cin>>T;
+    while(T--){
+        cin>>N>>M>>C;
+        fill(mins,mins+N+1,1e9);
+        
+        vector<int> cost(M+1),order(N+1);
+        for(int i = 1; i <= M; i++){
+            cin>>cost[i];
+        }
+        for(int i =1; i <= N; i++){
+            cin>>order[i];
+        }
+        dp[0][0]=0;
+        mins[0]=0;
+        for(int j = 1; j <= M; j++){
+            dp[0][j]=C;
+        }
+        for(int i = 1; i <= N; i++){
+            for(int j = 0; j <= M; j++){
+                if(order[i]==j){
+                    dp[i][j]=min(mins[i-1]+C, dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=min(dp[i-1][j]+cost[order[i]], mins[i-1]+C+cost[order[i]]);
+                }
+                //if(i==2)cout << j << ' ' << dp[i][j] << ' ' << mins[i-1] << endl;
+                mins[i]=min(mins[i],dp[i][j]);
+            }
+        }
+        cout << mins[N] << '\n';
+    }
+}
