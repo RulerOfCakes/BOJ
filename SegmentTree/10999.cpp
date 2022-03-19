aaa08cae6d94b81651dbe220daeb94a8938a28f8
RulerOfCakes <fragmentbombs@gmail.com>
2022-03-20 00:02:44 +0900

    boj 10999 - lazy

---
+++ SegmentTree/10999.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int N,M,K;
+
+class segtree {
+public:
+	vector<long long> tree;
+	vector<long long> lazy; // lazy propagataion
+	int size;
+
+	segtree (int n) {
+		for (size = 1; size < n; size *= 2);
+		tree.resize(size * 2, 0);
+		lazy.resize(size * 2);
+	}
+	void update(int node, long long x) {
+		int idx = size + node - 1;
+		long long diff = x - tree[idx];
+		while (idx) {
+			tree[idx] += diff;
+			idx /= 2;
+		}
+	}
+	void update_lazy(int node, int start, int end) {
+		if (!lazy[node])return;
+		tree[node] += lazy[node] * (end - start + 1);
+		if (start != end) {
+			lazy[node * 2] += lazy[node];
+			lazy[node * 2 + 1] += lazy[node];
+		}
+		lazy[node] = 0;
+	}
+	void update_range(int node, int nStart, int nEnd, int rStart, int rEnd, ll diff) {
+		update_lazy(node, nStart, nEnd);
+		if (nEnd < rStart || nStart > rEnd)return;
+		if (nStart >= rStart && nEnd <= rEnd) {
+			lazy[node] = diff;
+			update_lazy(node, nStart, nEnd);
+			return;
+		}
+		int mid = (nStart + nEnd) / 2;
+		update_range(node*2, nStart, mid, rStart, rEnd, diff);
+		update_range(node*2+1, mid + 1, nEnd, rStart, rEnd, diff);
+		tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	}
+	long long getsum(int node, int nStart, int nEnd, int rStart, int rEnd) {
+		update_lazy(node, nStart, nEnd);
+		if (nEnd < rStart || nStart > rEnd) return 0;
+		if (rStart <= nStart && rEnd >= nEnd) return tree[node];
+		int mid = (nStart + nEnd) / 2;
+		return getsum(node * 2, nStart, mid, rStart, rEnd) + getsum(node * 2 + 1, mid+1, nEnd, rStart, rEnd);
+	}
+};
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cin>>N>>M>>K;
+    segtree seg(N+2);
+    for(int i = 0; i < N; i++){
+        ll a;
+        cin>>a;
+        seg.update(i+1,a);
+    }
+    for(int i = 0; i < M+K; i++){
+        int a,b,c;ll d;
+        cin>>a>>b>>c;
+        if(a==1){
+            cin>>d;
+            seg.update_range(1,1,seg.size,b,c,d);
+        }
+        else{
+            cout << seg.getsum(1,1,seg.size,b,c) << '\n';
+        }
+    }
+}
